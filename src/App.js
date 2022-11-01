19e4ea9499f09a82995dc8e3528bd5df11bc30b2
Nathan Sonnenberg <65731867+ntsonnenberg@users.noreply.github.com>
2022-10-31 18:05:56 -0600

    configured and started learning tailwind

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from "./Footer";
 
 function App() {
 	return (
-		<div>
+		<div className="flex flex-col ...">
 			<Header />
 			<Content />
 			<Footer />
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,25 @@
 const Header = () => {
 	return (
 		<header>
-			<h1>Header</h1>
+			<div className="flex">
+				<div className="flex-auto">
+					<div className="relative left-10 top-10">
+						<h1>Nathan Sonnenberg</h1>
+					</div>
+				</div>
+				<div className="flex-auto">
+					<h3>About Me</h3>
+				</div>
+				<div className="flex-auto">
+					<h3>Business Experience</h3>
+				</div>
+				<div className="flex-auto">
+					<h3>Sonnenberg Group</h3>
+				</div>
+				<div className="flex-auto">
+					<h3>Mission</h3>
+				</div>
+			</div>
 		</header>
 	);
 };
